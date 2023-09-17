a9800fb7f1f5919d7c5f629b797e425bc736c3f9
zhursvlevy <zhursvlevy@mail.ru>
2023-09-17 19:21:23 +0300

    add train module

---
+++ src/train.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+import torch
+from torch.utils.data import DataLoader
+
+from omegaconf import OmegaConf
+from .data.ba_ru_dataset import BaRuDataset
+from transformers import T5ForConditionalGeneration, T5Config, T5TokenizerFast
+
+from dataclasses import dataclass
+
+@dataclass
+class DataConfig:
+    train_data: str
+    val_data: str
+    batch_size: int = 4
+    shuffle: bool = True
+
+@dataclass
+class ExperimentConfig:
+    data: DataConfig
+
+def train(cfg: ExperimentConfig):
+
+    train_dataset = BaRuDataset(cfg.data.train_data)
+    val_dataset = BaRuDataset(cfg.data.val_data)
+
+    train_loader = DataLoader(train_dataset, batch_size=cfg.data.batch_size)
+    val_loader = DataLoader(val_dataset, batch_size=cfg.data.batch_size)
+
+    print(next(iter(train_loader)))
+
+
+if __name__ == "__main__":
+
+    experiment_config = ExperimentConfig(DataConfig(
+        train_data="data/dataset/splits/train.parquet",
+        val_data="data/dataset/splits/val.parquet"
+    ))
+
+    train(experiment_config)
